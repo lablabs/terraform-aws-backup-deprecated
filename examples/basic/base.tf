e6e7b1c04c29a5126fd200fb6bb26b6692be1f5b
riqardos <kastiak.richard@gmail.com>
2023-02-06 17:20:00 +0100

    add example

--- examples/basic/base.tf
+++ examples/basic/base.tf
@@ -1,33 +1,24 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.14.2"
+# terraform apply --target aws_dynamodb_table.basic-dynamodb-table
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  provider       = aws.source
+  name           = "TestAWSBackup"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "UserId"
+  range_key      = "Title"
 
-  name               = "cluster-autoscaler-vpc"
-  cidr               = "10.0.0.0/16"
-  azs                = ["eu-central-1a", "eu-central-1b"]
-  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24"]
-  enable_nat_gateway = true
-}
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
 
-module "eks_cluster" {
-  source  = "cloudposse/eks-cluster/aws"
-  version = "2.3.0"
+  attribute {
+    name = "Title"
+    type = "S"
+  }
 
-  region     = "eu-central-1"
-  subnet_ids = module.vpc.public_subnets
-  vpc_id     = module.vpc.vpc_id
-  name       = "basic-example"
-}
-
-module "eks_node_group" {
-  source  = "cloudposse/eks-node-group/aws"
-  version = "2.4.0"
-
-  cluster_name   = module.eks_cluster.eks_cluster_id
-  instance_types = ["t3.medium"]
-  subnet_ids     = module.vpc.public_subnets
-  min_size       = 1
-  desired_size   = 1
-  max_size       = 2
-  depends_on     = [module.eks_cluster.kubernetes_config_map_id]
+  tags = {
+    Name        = "TestAWSBackup"
+    Environment = "Dev"
+  }
 }
